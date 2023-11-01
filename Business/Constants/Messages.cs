7feea2945ae19bd79c0e9ee7de3cb61c0e12ac61
Elif Özker <111710264+elifozker@users.noreply.github.com>
2023-11-01 14:44:53 +0300

    Utilities result folder added

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAddded = "Car eklendi";
+        public static string CarNameInvalid = "Car ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string CarsListed = "Cars listelendi";
+    }
+}
