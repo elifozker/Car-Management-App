4032377d0a296fffef1be35422490250efe4f314
Elif Özker <111710264+elifozker@users.noreply.github.com>
2023-11-02 13:01:39 +0300

    messages added

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -8,18 +8,18 @@ namespace Business.Constants
 {
     public static class Messages
     {
-        public static string Added = "eklendi";
+        public static string Added = "Added";
         public static string Deleted = "Deleted";
         public static string Updated = "Updated";
         public static string NameInvalid = "İsmi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
-        public static string Listed = "listelendi";
+        public static string Listed = "Listelendi";
         public static string BrandNotFound = "Brand not found";
         public static string GetSucceed = "Get Succeed";
-        internal static string CompanyNameNull;
-        internal static string AddedCustomerNotFoundUser;
-        internal static string RentalUnsucceed;
-        internal static string RentalByRentalId;
-        internal static string RentalByCarId;
+        internal static string CompanyNameNull = "Company Name Null";
+        internal static string AddedCustomerNotFoundUser = "AddedCustomerNotFoundUser";
+        internal static string RentalUnsucceed = "RentalUnsucceed";
+        internal static string RentalByRentalId = "RentalByRentalId";
+        internal static string RentalByCarId = "RentalByCarId";
     }
 }
