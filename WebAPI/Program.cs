2f0a7a5e7e14227045bdda823abbc3d5c790c383
Elif Özker <111710264+elifozker@users.noreply.github.com>
2024-02-08 22:41:26 +0300

    singleton added

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,9 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using WebAPI.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +13,29 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Brand
+builder.Services.AddSingleton<IBrandService, BrandManager>();
+builder.Services.AddSingleton<IBrandDal, EfBrandDal>();
+
+//Car
+builder.Services.AddSingleton<ICarService, CarManager>();
+builder.Services.AddSingleton<ICarDal, EfCarDal>();
+
+//Color
+builder.Services.AddSingleton<IColorService, ColorManager>();
+builder.Services.AddSingleton<IColorDal, EfColorDal>();
+
+//Customer
+builder.Services.AddSingleton<ICustomerService, CustomerManager>();
+builder.Services.AddSingleton<ICustomerDal, EfCustomerDal>();
+
+//Rental
+builder.Services.AddSingleton<IRentalService, RentalManager>();
+builder.Services.AddSingleton<IRentalDal, EfRentalDal>();
+
+//User
+builder.Services.AddSingleton<IUserService, UserManager>();
+builder.Services.AddSingleton<IUserDal, EfUserDal>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
